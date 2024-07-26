0e9159d67f22d3b0c8f385bd99be8616e1467a07
tezuka0001 <167077689+tezuka0001@users.noreply.github.com>
2024-07-26 13:26:52 +0900

    Create demo_web

---
+++ demo_web.py
@@ -0,0 +1,14 @@
+import gradio as gr
+
+def read_file(file):
+    with open(file.name, "r", encoding="utf-8") as f:
+        content = f.read()
+    return content
+
+iface = gr.Interface(
+    fn=read_file, 
+    inputs=gr.inputs.File(type="file", label="Upload a text file"), 
+    outputs=gr.outputs.Textbox(label="File content")
+)
+
+iface.launch()
